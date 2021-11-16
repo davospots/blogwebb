2cca8cf6d1790d53efe2254b47c3550d2c0ad0f4
Dave <david.mathaga@student.moringaschool.com>
2021-11-16 06:03:26 +0300

    fixing database error

--- website/__init__.py
+++ website/__init__.py
@@ -9,8 +9,8 @@ DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = "helloworld"
-    app.config['SQL_ALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SECRET_KEY'] = 'eowqiepowqipeoeoqwpeoiwpqoei'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
     from .views import views
